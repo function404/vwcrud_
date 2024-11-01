48199811d2c40a7c4aad8bb8ed98b13429b1f746
Felipe-G-Schmitt <felipegabrielsc2006@gmail.com>
2024-11-01 20:22:40 -0300

    feat: add folder gfx with logo

--- assets/pages/signUpPage.php
+++ assets/pages/signUpPage.php
@@ -11,13 +11,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="../css/global.css">
-    <link rel="shortcut icon" type="image/x-icon" sizes="32x32" href="https://s3-alpha-sig.figma.com/img/aa4e/4b27/f2a0230cad97e6d4453b5aabf7dc383f?Expires=1731283200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=mTi0hPdRdSpzcc1aITmZZzJnSzm6bt97oLmYbazvc6sLfiGxS8eUIQTLgjM0Q-Cxxmm4skkQH~aitODfDA-vHhJ6H2tZi8E~skMh6P9LP~NVvb6VUlzK36~s7kCd8DleOBISHXd~Ef51pb38Aat5HAz5WVEVtD2SfHk3U5TZy7tL7chf8MY63bmSNXDEf5ftvMhnLUNo2xidBjhECvZ8xiKgmaRckbscPDuGDA2pa7lhBMgo7SP56Vnk~Odvx4YNk2jFu8BG2xSv4N0gccFJpGaj5I0NWVJD58RNegjAW8eaZh0Um9nxtmxBmdJSdyFTcaXlncJBCU8Go4GlfoClCQ__" />
+    <link rel="shortcut icon" type="image/x-icon" href="../gfx/volkswagen-logo.png" />
     <title>VW: Volkswagen | Sign Up</title>
 </head>
 <body>
     <main>
         <div>
-            <h3><a href="homePage.php">LOGO VW</a></h3>
+            <a href="homePage.php">
+                <img style="width: 72px; height: 72px;" src="../gfx/volkswagen-logo.png" alt="LOGO-VW">    
+            </a>
         </div>
         <div>
             <h1>Sing Up page</h1>
